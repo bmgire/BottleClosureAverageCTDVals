7ebf1e489a525d11493d8e76d0876d36cef2987d
Ben Gire <bmgire@gmail.com>
2024-08-21 05:20:09 -0700

    Description update

--- BottleClosureAverageCTDVals.py
+++ BottleClosureAverageCTDVals.py
@@ -2,10 +2,10 @@
 ##################################################################################################
 # python program = BottleClosureAverageCTDVals.py
 #################################################
+# The program will average CTD data from bottle closures to 4 seconds (user editable) before each bottle closure.
 # This program will take derived CTD data that has been processed using SBE software per CalCOFI's guidelines. 
 # The derived data will already need to be in an .asc file (again per the CalCOFI instructions). 
 # You will also need to have the .bl file for the cast. 
-# The program will average data from bottle closure to 4 seconds (user editable) before the bottle closure.
 # It uses the .bl file to get the bottle closure scan number and is currently coded to use the beginning scan. 
 # It will produce a new .csv file with "_btlAvgd.csv" appended to the end - copying the formatting of the .asc filenaming structure. 
 # The new averaged CTD values can be used to compare with the chemically analyzed bottle values.    
